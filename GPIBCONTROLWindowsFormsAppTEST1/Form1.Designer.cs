8751cfa6103c0de3176f954cd661b28305e61c97
deg16 <isaka121670@gmail.com>
2024-11-14 16:58:36 +0900

    プロジェクト ファイルを追加します。

---
+++ GPIBCONTROLWindowsFormsAppTEST1/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace GPIBCONTROLWindowsFormsAppTEST1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(586, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(49, 73);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(471, 19);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox2.Location = new System.Drawing.Point(49, 108);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(471, 19);
+            this.textBox2.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(74, 152);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 30);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(231, 152);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 30);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Location = new System.Drawing.Point(60, 219);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(628, 222);
+            this.chart1.TabIndex = 9;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 573);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ GPIBCONTROLWindowsFormsAppTEST1/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NationalInstruments.Visa;
+using Ivi.Visa;
+using System.Threading;
+
+using System.Windows.Forms.DataVisualization.Charting;  //chart用
+
+
+
+namespace GPIBCONTROLWindowsFormsAppTEST1
+{
+    public partial class Form1 : Form
+    {
+        private MessageBasedSession mbSession;      //VISAセッションをするためのもの
+        private System.Windows.Forms.Timer timer;　//TimerかつThreading（UIに出さないよう）とかぶらないため
+
+        //Chart
+        private Series series;
+        private int displayRange = 20;  // 表示範囲のデータ数
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // タイマーの初期化
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 1000; // 1秒ごとにイベントを発生させる
+            timer.Tick += timer1_Tick;
+
+            // Chartコントロールの初期化
+            series = new Series("Data");
+            series.ChartType = SeriesChartType.Line; // 点として表示
+            chart1.Series.Add(series);
+
+            // チャートの軸の設定
+            chart1.ChartAreas[0].AxisX.Title = "Time (seconds)";
+            chart1.ChartAreas[0].AxisY.Title = "Voltage";
+
+            // X軸の設定
+            chart1.ChartAreas[0].AxisX.Minimum = 0;  // X軸の初期値
+            chart1.ChartAreas[0].AxisX.Maximum = displayRange - 1 ;  // 初期表示範囲を設定 
+
+            chart1.ChartAreas[0].AxisX.Interval = 5;  // X軸の目盛りの間隔            
+            chart1.ChartAreas[0].AxisX.IsMarginVisible = false;     //X軸の余白をなくす
+
+        }
+        
+        //機器との通信、個別にコマンドでトークする用
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var rmSession = new ResourceManager())
+            {
+                try
+                {
+                    mbSession = (MessageBasedSession)rmSession.Open("GPIB0::2::INSTR");
+                    mbSession.RawIO.Write(textBox1.Text);
+                    textBox2.Text = mbSession.RawIO.ReadString();
+                    mbSession.Dispose();
+                }
+                catch
+                {
+                    MessageBox.Show("通信エラー");
+                }
+            }
+        }
+
+        //電圧の測定
+        private void button2_Click(object sender, EventArgs e)  //電圧計測開始ボタン
+        {
+            timer1.Start();        
+        }
+
+        private void button3_Click(object sender, EventArgs e)  //停止ボタン
+        {
+            timer1.Enabled = false;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            using (var rmSession = new ResourceManager())   //変数の宣言
+             try
+            {
+                    //DMMから電圧を得る
+                mbSession = (MessageBasedSession)rmSession.Open("GPIB0::3::INSTR");  //特定の機器への挨拶
+                mbSession.RawIO.Write("MEAS:VOLT:DC?");     //DC(直流）電圧測定のWrite
+               // mbSession.RawIO.Write("MEAS:VOLT:AC?");       //AC(交流）電圧測定のWrite
+                Thread.Sleep(TimeSpan.FromSeconds(1));   //Read,Write間隔
+                string voltageData = mbSession.RawIO.ReadString();
+                mbSession.Dispose();
+
+                    // グラフにデータを追加
+                double voltageValue = double.Parse(voltageData);
+                series.Points.AddY(voltageValue);
+
+                //    // UIの更新
+                UpdateChart(voltageValue);
+             }
+             catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                timer1.Stop();
+            }
+        }
+
+        //Chartを右から左へ流れるように更新する
+        private void UpdateChart(double voltageValue)
+        {
+            int lastPointIndex = series.Points.Count - 1;
+            //int displayRange = 20;  // 表示範囲のデータ数
+
+            // データポイント数が表示範囲を超えている場合、表示範囲を動的に更新
+            if (lastPointIndex > displayRange)
+            {
+                chart1.ChartAreas[0].AxisX.Minimum = lastPointIndex - displayRange;
+                chart1.ChartAreas[0].AxisX.Maximum = lastPointIndex;
+            }
+        }
+    }
+}
